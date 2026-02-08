ffaf743f16499729a75716f7336231b619972218
atelier-eureka <kuya.ed20@gmail.com>
2026-02-08 20:42:47 +0800

    Add custom font

--- main.js
+++ main.js
@@ -2,10 +2,28 @@ async function fetchGameUpdates() {
     const response = await fetch('https://api.github.com/repos/kuyaed-1720/my-pixi-game/commits');
     const commits = await response.json();
 
+    let lastDate = '';
+
     const list = document.getElementById('commit-list');
     commits.slice(0, 10).forEach(commit => {
+        const currentDate = commit.commit.author.date.split('T')[0];
+        const message = commit.commit.message;
+
         const li = document.createElement('li');
-        li.innerHTML = `<b>${commit.commit.author.date.split('T')[0]}<b>: ${commit.commit.message}`;
+        li.style.listStyle = 'none';
+
+        if (currentDate !== lastDate) {
+            li.innerHTML = `
+                <div style="margin-top: 20px; font-weight: bold;">${currentDate}</div>
+                <div style="margin-bottom: 10px;">${"-".repeat(40)} ${message}</div>
+            `;
+            lastDate = currentDate;
+        } else {
+            li.innerHTML = `
+                <div style="margin-bottom: 10px;">${"-".repeat(40)} ${message}</div>
+            `;
+        }
+
         list.appendChild(li);
     });
 }
